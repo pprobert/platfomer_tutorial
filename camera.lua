8f0fa1d31f7ff93497a0031ea0e128a654301f3b
pprobert <pwprobert@gmail.com>
2025-04-25 11:39:13 -0600

    first commit

---
+++ camera.lua
@@ -0,0 +1,24 @@
+
+
+local Camera = {
+    x = 0,
+    y = 0, 
+    scale = 2
+}
+
+function Camera:apply()
+    love.graphics.push()
+    love.graphics.scale(self.scale, self.scale)
+    love.graphics.translate(-self.x, -self.y)
+end
+
+function Camera:clear()
+    love.graphics.pop()
+end
+
+function Camera:setPosition(x, y)
+    self.x = x - love.graphics.getWidth() / 2 / self.scale
+    self.y = y 
+end
+
+return Camera
---
+++ main.lua
@@ -0,0 +1,62 @@
+love.graphics.setDefaultFilter("nearest", "nearest")
+local STI = require("sti")
+local Player = require("player")
+local Coin = require("coin")
+local GUI = require("gui")
+local Spike = require("spikes")
+local Camera = require("camera")
+
+function love.load()
+    Map = STI("map/firstMap.lua", {"box2d"})
+    World = love.physics.newWorld(0, 0)
+    World:setCallbacks(beginContact, endContact)
+    Map:box2d_init(World)
+    Map.layers.solid.visible = false
+    background = love.graphics.newImage("assests/background.png")
+    GUI:load()
+    Player:load()
+    Coin.new(300, 200)
+    Coin.new(400, 200)
+    Coin.new(500, 200)
+    Spike.new(500, 325)
+end
+
+function love.update(dt)
+    World:update(dt)
+    Player:update(dt)
+    Coin.updateAll(dt)
+    Spike.updateAll(dt)
+    GUI:update(dt)
+    Camera:setPosition(Player.x, 0)
+    
+end
+
+function love.draw()
+    love.graphics.draw(background)
+    Map:draw(-Camera.x, -Camera.y, Camera.scale, Camera.scale)
+    love.graphics.push()
+    love.graphics.scale(2, 2)
+
+    Camera:apply()
+    Player:draw()
+    Coin.drawAll()
+    Spike.drawAll()
+    Camera:clear()
+
+    love.graphics.pop()
+    GUI:draw()
+end
+
+function love.keypressed(key)
+    Player:jump(key)
+end
+
+function beginContact(a, b, collision)
+    if Coin.beginContact(a, b, collision) then return end
+    if Spike.beginContact(a, b, collision) then return end
+    Player:beginContact(a, b, collision)
+end
+
+function endContact(a, b, collision)
+    Player:endContact(a, b, collision)
+end
